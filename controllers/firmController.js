ddddb7450acc9cc2ca74b8a369b8ebc421a118fb
SIMHADRIMALLAPURAM <mallapuramsimhadri879@gmail.com>
2026-06-16 14:12:31 +0530

    first time pushing my project to git

---
+++ controllers/firmController.js
@@ -0,0 +1,81 @@
+
+const Firm = require('../models/Firm');
+const Vendor = require('../models/vendor');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname( file.originalname));
+    }
+});
+
+const upload = multer({ storage });
+
+const addFirm = async (req, res) => {
+    try {
+        const { firmName, area, category, region, offer } = req.body;
+
+        const image = req.file ? req.file.filename : undefined;
+
+        console.log("Vendor ID:", req.vendorId);
+
+        const vendor = await Vendor.findById(req.vendorId);
+
+        if (!vendor) {
+            return res.status(404).json({
+                message: "Vendor not found"
+            });
+        }
+
+        const firm = new Firm({
+            firmName,
+            area,
+            category,
+            region,
+            offer,
+            image,
+            vendor: vendor._id
+        });
+
+        const savedFirm = await firm.save();
+
+        vendor.firm.push(savedFirm)
+
+        await vendor.save()
+
+        return res.status(201).json({
+            message: "Firm Added Successfully",
+            firm
+        });
+
+    } catch (error) {
+        console.error("Add Firm Error:", error);
+
+        return res.status(500).json({
+            message: "Internal Server Error"
+        });
+    }
+};
+
+const deleteFirmById = async(req, res)=>{
+    try {
+           const firmId = req.params.firmId;
+
+           const deletedFirm = await Firm.findByIdAndDelete(firmId);
+
+           if(!deletedFirm){
+            return res.status(404).json({error: "No firm found"})
+           }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal server error" });
+        
+    }
+}
+
+module.exports = {
+    addFirm: [upload.single('image'), addFirm], deleteFirmById
+};
